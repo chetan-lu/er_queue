b808969db97db49a5219bd8cd7bb1a7ebf409fde
hkjanda <35811365+hkjanda@users.noreply.github.com>
2018-03-25 22:23:31 -0400

    Adding updateViewController.js
    Adding seed file

---
+++ controllers/updateViewController.js
@@ -0,0 +1,23 @@
+'use strict';
+app.controller('updateViewController', function($scope, $http, ModalService) {
+	Array.prototype.remove = function(from, to) {
+		var rest = this.slice((to || from) + 1 || this.length);
+		this.length = from < 0 ? this.length + from : from;
+		return this.push.apply(this, rest);
+	};
+	$scope.triage = function(id, action) {
+		//$scope.patients.remove(id);
+		$http.post('/api/patient', {'_id': id, priority: action}, {}).then(function(response) {});
+	};
+	$http.get("/api/queue").then(function(response) {
+		$scope.patients = response.data;
+		console.log(response.data);
+	});
+	var socket = io();
+	socket.on('broadcast', function(data) {
+		console.log('Hello');
+		$http.get("/api/queue").then(function(response) {
+			$scope.patients = response.data;
+		});
+	});
+});
